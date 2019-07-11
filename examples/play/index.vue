78926e25ce65cc7ee2e132a4278356d25f9bb369
qinjunshan <13782533791@163.com>
2019-07-11 14:26:37 +0800

    badge;breadcrumb;breadcrumb-item

--- examples/play/index.vue
+++ examples/play/index.vue
@@ -2,7 +2,12 @@
 <section class="test" style="height: 500px;overflow:auto;">
   <div style="height: 1200px;">
     <div>test</div>
-    <el-backtop target=".test"></el-backtop>
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item><a href="/">活动管理</a></el-breadcrumb-item>
+      <el-breadcrumb-item>活动列表</el-breadcrumb-item>
+      <el-breadcrumb-item>活动详情</el-breadcrumb-item>
+    </el-breadcrumb>
   </div>
 </section>
 </template>
